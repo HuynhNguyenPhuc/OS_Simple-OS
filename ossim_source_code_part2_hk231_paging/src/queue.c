20cbbbd4b4dc0f8d7692e861d3efbc0b730db0b7
Quan Cao <quancao2310@gmail.com>
2023-05-05 00:11:22 +0700

    runnable, but incorrect

---
+++ ossim_source_code_part2_hk231_paging/src/queue.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t * q) {
+        if (q == NULL) return 1;
+        return (q->size == 0);
+}
+
+void enqueue(struct queue_t * q, struct pcb_t * proc) {
+        /* TODO: put a new process to queue [q] */
+        if (proc == NULL || q->size == MAX_QUEUE_SIZE) return;
+        q->proc[q->size] = proc;
+        q->size++;
+}
+
+struct pcb_t * dequeue(struct queue_t * q) {
+        /* TODO: return a pcb whose prioprity is the highest
+         * in the queue [q] and remember to remove it from q
+         * */
+        if (empty(q)) return NULL;
+
+        int idx = 0;
+        uint32_t max_priority = q->proc[0]->priority;
+        for (int i = 1; i < q->size; i++) {
+                if (q->proc[i]->priority > max_priority) {
+                        idx = i;
+                        max_priority = q->proc[i]->priority;
+                }
+        }
+
+        struct pcb_t *proc = q->proc[idx];
+        for (int i = idx; i < q->size-1; i++) q->proc[i] = q->proc[i+1];
+        q->size--;
+        return proc;
+}
+
